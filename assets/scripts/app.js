464f1b2eec99fcfe6e4005ab3755547c062731fc
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2020-09-16 14:43:03 +0300

    add all the calculating operator functions and finalize the project

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,59 +1,111 @@
 const defaultResult = 0;
 
 let currentResult = defaultResult;
+let logEntries = [];
 function getUserNumberInput() {
   return parseInt(userInput.value);
 }
+
+
 // Generate and writes calculation log
 function createandWriteOutput(operator, resultBeforecalc, calcNumber) {
   const calcDesecription = `${resultBeforecalc} ${operator}  ${calcNumber}`;
   outputResult(currentResult, calcDesecription);
 }
 
+function logEntryFunction(
+  operationIdentifier,
+  calculatedResult,
+  declaredNumber,
+  operatedResult,
+) {
+  // create  object to collect the data
+  const logEntry = {
+    operation: operationIdentifier,
+    previousResult: calculatedResult,
+    insertedNumber: declaredNumber,
+    result: operatedResult,
+  };
+
+  // Array to store the object in
+  logEntries.push(logEntry);
+  console.log(logEntry.operation);
+  console.log(logEntries);
+}
+
 function addNumber() {
   const enteredNum = getUserNumberInput();
 
-/* gets inputs from input feild and parse it to integer  */
-  currentResult = +currentResult + +enteredNum;
-  const initialResult = currentResult;
+  if (userInput.value == '') {
+    console.log(' Empty text Field!!! ');
+    alert('Please insert value!');
+    return;
+  }
 
+  const initialResult = currentResult;
+  /* gets inputs from input feild and parse it to integer  */
+  currentResult += +enteredNum;
   // retreived from Vendor.js file
-  createandWriteOutput(" + ", initialResult, enteredNum);
+  createandWriteOutput(' + ', initialResult, enteredNum);
+
+  // function include the Object and the array to avoid repetation
+  logEntryFunction(' ADD  ', initialResult, enteredNum, currentResult);
 }
 
 function subtractNumber() {
   const enteredNum = getUserNumberInput();
 
+  if (userInput.value == '') {
+    console.log(' Empty text Field!!! ');
+    alert('insert value :)');
+    return;
+  }
   const initialResult = currentResult;
 
-  currentResult = +currentResult - +enteredNum;
+  currentResult -= +enteredNum;
 
-  createandWriteOutput(" - ", initialResult, enteredNum);
+  createandWriteOutput(' - ', initialResult, enteredNum);
+
+  logEntryFunction(' SUBTRACT ', initialResult, enteredNum, currentResult);
 }
 
 function multiplyNumber() {
   const enteredNum = getUserNumberInput();
+  if (userInput.value == '') {
+    console.log(' Empty text Field!!! ');
+    alert('insert value :)');
+    return;
+  }
 
   const initialResult = currentResult;
 
-  currentResult = +currentResult * +enteredNum;
-  createandWriteOutput(" * ", initialResult, enteredNum);
+  currentResult *= +enteredNum;
+  createandWriteOutput(' * ', initialResult, enteredNum);
+
+  logEntryFunction(' MULTIPLY ', initialResult, enteredNum, currentResult);
 }
 
 function devidedNumber() {
   const enteredNum = getUserNumberInput();
+  if (userInput.value == '') {
+    console.log(' Empty text Field!!! ');
+    alert('insert value :)');
+    return;
+  }
 
   const initialResult = currentResult;
 
-  currentResult = +currentResult / +enteredNum;
+  currentResult /= +enteredNum;
 
-  createandWriteOutput(" / ", initialResult, enteredNum);
+  createandWriteOutput(' / ', initialResult, enteredNum);
+
+  logEntryFunction(' DEVISION ', initialResult, enteredNum, currentResult);
 }
 
-addBtn.addEventListener("click", addNumber);
+addBtn.addEventListener('click', addNumber);
 
-subtractBtn.addEventListener("click", subtractNumber);
+subtractBtn.addEventListener('click', subtractNumber);
 
-multiplyBtn.addEventListener("click", multiplyNumber);
+multiplyBtn.addEventListener('click', multiplyNumber);
 
-divideBtn.addEventListener("click", devidedNumber);
+divideBtn.addEventListener('click', devidedNumber);
